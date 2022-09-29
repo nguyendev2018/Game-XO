083be424a67092e954e7b750ee2234de9027c840
nguyendev2018 <nguyendev2018@gmail.con>
2022-09-29 10:15:28 +0700

    --- Auto Git Commit ---

--- js/constants.js
+++ js/constants.js
@@ -1,16 +1,15 @@
 export const TURN = {
-    CROSS: "cross",
-    CIRCLE: "circle",
-
-}
+  CROSS: "cross",
+  CIRCLE: "circle",
+};
 export const STATUS = {
-    PLAY: "PLAY",
-    ENDGAME: "ENDGAME",
-    X_WIN: "X WIN",
-    O_WIN: "O WIN",
-}
+  PLAY: "PLAY",
+  ENDGAME: "ENDGAME",
+  X_WIN: "X WIN",
+  O_WIN: "O WIN",
+};
 export const itemTable = {
-    CROSS: "X",
-    CIRCLE: "O",
-    ITEM_WIN: "win"
-}
+  CROSS: "X",
+  CIRCLE: "O",
+  ITEM_WIN: "win",
+};
